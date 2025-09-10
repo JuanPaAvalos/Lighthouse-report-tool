6dbb807f4f744a5e971f034501a6d69cfd3dc9d2
JuanPaAvalos <135082940+JuanPaAvalos@users.noreply.github.com>
2025-09-10 09:48:21 -0500

    update response result to object Array with mobile and desktop data

--- index.js
+++ index.js
@@ -1,123 +1,186 @@
-import { writeFileSync } from 'fs';  // For file writing
-import { parse } from 'json2csv';
-import dotenv from 'dotenv';
+import { writeFileSync } from 'fs' // For file writing
+import { parse } from 'json2csv'
+import dotenv from 'dotenv'
+import { url } from 'inspector'
 
-dotenv.config();
+dotenv.config()
 
-const API_KEY = "AIzaSyBCJs3zOZXXDdSCMVEkuJPEOMXr1zsHQgY";
-const strategies = ['MOBILE', 'DESKTOP'];
-const categories = ['ACCESSIBILITY', 'BEST_PRACTICES', 'PERFORMANCE', 'SEO'];
+const API_KEY = 'AIzaSyBCJs3zOZXXDdSCMVEkuJPEOMXr1zsHQgY'
+const strategies = ['MOBILE', 'DESKTOP']
+const categories = ['ACCESSIBILITY', 'BEST_PRACTICES', 'PERFORMANCE', 'SEO']
 
 let urls = [
-    'https://www.riotgames.com/en/',
-    'https://hollowknightsilksong.com/',
-];
+  'https://www.riotgames.com/en/',
+  'https://hollowknightsilksong.com/'
+]
 
 //* Create urls for lighthouse based on current values
+// return an arrat of
+// {
+//     original: string,
+//     mobile: URL,
+//     desktop: URL
+// }
 const createFetchUrls = () => {
-    let resultUrls = [];
+  let resultUrls = []
+
+  for (const url of urls) {
+    let urlObj = {
+      mobile: '',
+      desktop: '',
+      original: url
+    }
 
     for (const strategy of strategies) {
-        for (const url of urls) {
-            const u = new URL('https://pagespeedonline.googleapis.com/pagespeedonline/v5/runPagespeed');
-            u.searchParams.set('key', API_KEY);
-            u.searchParams.set('url', url);
-            u.searchParams.set('strategy', strategy);
-            categories.forEach(category => {
-                u.searchParams.append('category', category);
-            });
+      const u = new URL(
+        'https://pagespeedonline.googleapis.com/pagespeedonline/v5/runPagespeed'
+      )
+      u.searchParams.set('key', API_KEY)
+      u.searchParams.set('url', url)
+      u.searchParams.set('strategy', strategy)
+      categories.forEach(category => {
+        u.searchParams.append('category', category)
+      })
 
-            resultUrls.push(u);
-        }
+      if (strategy === 'MOBILE') urlObj.mobile = u
+      if (strategy === 'DESKTOP') urlObj.desktop = u
     }
 
-    return resultUrls;
-};
+    resultUrls.push(urlObj)
+  }
+
+  return resultUrls
+}
 
 //* Simple fetch to call API
-const getData = async (evaluationUrl) => {
-    try {
-        const response = await fetch(evaluationUrl);
+// * return the json result or null if error
+const getData = async evaluationUrl => {
+  try {
+    const response = await fetch(evaluationUrl)
 
-        if (!response.ok) {
-            throw new Error(`Response status: ${response.status}`);
-        }
-
-        const result = await response.json();
-        return result;
-    } catch (error) {
-        console.error(`Error fetching data: ${error.message}`);
-        return null;
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`)
     }
+
+    const result = await response.json()
+    return result
+  } catch (error) {
+    console.error(`Error fetching data: ${error.message}`)
+    return null
+  }
 }
 
-//* Object property for csv fields
-const extractFields = (reportData) => {
-    return {
-        initialUrl: reportData.loadingExperience.initial_url,
-        finalUrl: reportData.lighthouseResult.finalUrl,
-        emulatedDevice: reportData.lighthouseResult.configSettings.emulatedFormFactor,
-        performanceScore: reportData.lighthouseResult.categories.performance.score,
-        accessibilityScore: reportData.lighthouseResult.categories.accessibility.score,
-        bestPracticesScore: reportData.lighthouseResult.categories['best-practices'].score,
-        seoScore: reportData.lighthouseResult.categories.seo.score,
-    };
-};
+//* Object for easy access to fields
+const extractFields = reportData => {
+  return {
+    initialUrl: reportData.loadingExperience.initial_url,
+    finalUrl: reportData.lighthouseResult.finalUrl,
+    emulatedDevice:
+      reportData.lighthouseResult.configSettings.emulatedFormFactor,
+    performanceScore: reportData.lighthouseResult.categories.performance.score,
+    accessibilityScore:
+      reportData.lighthouseResult.categories.accessibility.score,
+    bestPracticesScore:
+      reportData.lighthouseResult.categories['best-practices'].score,
+    seoScore: reportData.lighthouseResult.categories.seo.score
+  }
+}
+
+//* Object for Json result
+const createReportObject = (originalUrl, mobileData, desktopData) => {
+  const mobileObjData = mobileData ? extractFields(mobileData) : {}
+  const desktopObjData = desktopData ? extractFields(desktopData) : {}
+
+  return {
+    url: originalUrl,
+    mobile_performance: mobileObjData.performanceScore || '',
+    mobile_accessibility: mobileObjData.accessibilityScore || '',
+    mobile_best_practices: mobileObjData.bestPracticesScore || '',
+    mobile_seo: mobileObjData.seoScore || '',
+    desktop_performance: desktopObjData.performanceScore || '',
+    desktop_accessibility: desktopObjData.accessibilityScore || '',
+    desktop_best_practices: desktopObjData.bestPracticesScore || '',
+    desktop_seo: desktopObjData.seoScore || ''
+  }
+}
 
 const generateFileName = () => {
-    const now = new Date();
+  const now = new Date()
 
-    const year = now.getFullYear();
-    const month = String(now.getMonth() + 1).padStart(2, '0'); // 0-11 → 1-12
-    const day = String(now.getDate()).padStart(2, '0');
-    const hours = String(now.getHours()).padStart(2, '0');
-    const minutes = String(now.getMinutes()).padStart(2, '0');
+  const year = now.getFullYear()
+  const month = String(now.getMonth() + 1).padStart(2, '0') // 0-11 → 1-12
+  const day = String(now.getDate()).padStart(2, '0')
+  const hours = String(now.getHours()).padStart(2, '0')
+  const minutes = String(now.getMinutes()).padStart(2, '0')
 
-    return `${year}-${month}-${day}-${hours}-${minutes}`;
+  return `${year}-${month}-${day}-${hours}-${minutes}`
 }
 
-async function run() {
-    let nonResult = [];
-    let successfulResults = [];
+async function run () {
+  let nonResult = []
+  let reportsArray = []
 
-    const urls = createFetchUrls();
+  // arreglo de objetos con las urls para evaluar mobile y desktop
+  const urlArray = createFetchUrls()
 
-    let iteration = 1;
-    for (const url of urls) {
-        console.clear();
-        console.log(`Realizando el reporte ${iteration} de ${urls.length}`);
-        const reportData = await getData(url);
+  let iteration = 1
+  for (const urlObj of urlArray) {
+    console.clear()
+    console.log(`Realizando el reporte ${iteration} de ${urlArray.length}`)
 
-        if (!reportData) {
-            const evaluatedUrl = url.searchParams.get('url');
-            if (!nonResult.includes(evaluatedUrl)) {
-                nonResult.push(evaluatedUrl);
-            }
-            iteration++;
-            continue;
-        }
+    // Fetch data result might be JSON or null
+    const mobileResult = await getData(urlObj.mobile)
+    const desktopResult = await getData(urlObj.desktop)
 
-        const extractedData = extractFields(reportData);
-        successfulResults.push(extractedData);
-        iteration++;
+    if (!mobileResult) {
+      const evaluatedUrl = {
+        url: urlObj.original,
+        device: 'MOBILE'
+      }
+      nonResult.push(evaluatedUrl)
     }
 
-    if (nonResult.length > 0) {
-        console.log("URLs with no results:");
-        nonResult.forEach(url => console.log(url.toString()))
+    if (!desktopResult) {
+      const evaluatedUrl = {
+        url: urlObj.original,
+        device: 'DESKTOP'
+      }
+      nonResult.push(evaluatedUrl)
     }
 
-    if (successfulResults.length > 0) {
-        const csv = parse(successfulResults);
-        const filename = generateFileName()
-        const filePath = './lighthouse_reports.csv';
+    const reportResult = createReportObject(
+      urlObj.original,
+      mobileResult,
+      desktopResult
+    )
 
-        writeFileSync(filePath, csv);
-        console.log(`CSV file saved to ${filePath}`);
-    } else {
-        console.log('No successful results to write to CSV.');
-    }
+    reportsArray.push(reportResult)
+    iteration++
+  }
 
+  const finalResult = {
+    nonResult: nonResult,
+    reports: reportsArray
+  }
+
+  console.log(JSON.stringify(finalResult, null, 2))
+
+  //*  CODE FOR CSV FILE GENERATION
+  //   if (nonResult.length > 0) {
+  //     console.log('URLs with no results:')
+  //     nonResult.forEach(url => console.log(url.toString()))
+  //   }
+
+  //   if (reportsArray.length > 0) {
+  //     const csv = parse(reportsArray)
+  //     const filename = generateFileName()
+  //     const filePath = './lighthouse_reports.csv'
+
+  //     writeFileSync(filePath, csv)
+  //     console.log(`CSV file saved to ${filePath}`)
+  //   } else {
+  //     console.log('No successful results to write to CSV.')
+  //   }
 }
 
 run()
