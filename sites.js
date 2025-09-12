f1c642d79654dd7c8ca70415d8bbdccccc1be9b4
JuanPaAvalos <135082940+JuanPaAvalos@users.noreply.github.com>
2025-09-12 10:13:20 -0500

    add sites constant

---
+++ sites.js
@@ -0,0 +1,5 @@
+export const SitesUrls = [
+  { ruta: "Buscar hotel", pantalla: "home", url: "https://www.pricetravel.com/" },
+  { ruta: "Buscar hotel", pantalla: "lista de hoteles", url: "https://www.pricetravel.com/en/hotels-in-new-york_d55976" },
+  // { ruta: "Buscar hotel", pantalla: "checkout", url: "https://www.pricetravel.com/en/deals/top-10-family-friendly-hotels/" }
+];
